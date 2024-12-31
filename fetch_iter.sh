4675fe954016d69ef261570c1239701469708b8e
s0urce-c0de <email.kanavg@gmail.com>
2024-12-31 07:03:53 +0000

    Now the final test (will prob fail)

--- fetch_iter.sh
+++ fetch_iter.sh
@@ -7,5 +7,4 @@ do
   gh release list --json tagName,isDraft,isPrerelease | \
   jq -e "map(select(.isDraft==false and .isPrerelease==false and .tagName==\"$(printf $i | b64jq .file | awk -F '_' '{print $2}')-$(printf $i | b64jq -r .chrome_version)\"))==[]" >/dev/null && \
   env $(printf $i | b64jq -rcM '[. | to_entries | .[] | (.key | ascii_upcase) + "=" + .value] | join(" ")') ./process_info.sh
-  break
 done
