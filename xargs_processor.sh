20f5b2161a4eaf9cb0fa19e91198ed4cede54464
s0urce-c0de <email.kanavg@gmail.com>
2025-01-01 19:35:44 -0800

    tag name would still be taken

--- xargs_processor.sh
+++ xargs_processor.sh
@@ -2,5 +2,5 @@
 b64jq() { base64 -d | jq "$@"; }
 
 gh release list --json tagName,isDraft,isPrerelease | \
-jq -e "map(select(.isDraft==false and .isPrerelease==false and .tagName==\"$(printf "$1" | b64jq .file | awk -F '_' '{print $2}')/$(printf $1 | b64jq -r .channel)/$(printf $1 | b64jq -r .chrome_version)\"))==[]" >/dev/null && \
+jq -e "map(select(.tagName==\"$(printf "$1" | b64jq .file | awk -F '_' '{print $2}')/$(printf $1 | b64jq -r .channel)/$(printf $1 | b64jq -r .chrome_version)\"))==[]" >/dev/null && \
 env BOARD="$(printf "$1" | b64jq ".file" | awk -F _ '{print $3}')" $(printf $1 | b64jq -rcM '[. | to_entries | .[] | (.key | ascii_upcase) + "=" + .value] | join(" ")') ./process_info.sh
