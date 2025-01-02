de58fa981652da45c2c6e7ce77559efc79971b85
s0urce-c0de <email.kanavg@gmail.com>
2025-01-01 19:58:47 -0800

    Update xargs_processor.sh

--- xargs_processor.sh
+++ xargs_processor.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 b64jq() { base64 -d | jq "$@"; }
 
-gh release list --json tagName,isDraft,isPrerelease | \
+gh release list --json tagName | \
 jq -e "map(select(.tagName==\"$(printf "$1" | b64jq .file | awk -F '_' '{print $2}')/$(printf $1 | b64jq -r .channel)/$(printf $1 | b64jq -r .chrome_version)\"))==[]" >/dev/null && \
 env BOARD="$(printf "$1" | b64jq ".file" | awk -F _ '{print $3}')" $(printf $1 | b64jq -rcM '[. | to_entries | .[] | (.key | ascii_upcase) + "=" + .value] | join(" ")') ./process_info.sh
