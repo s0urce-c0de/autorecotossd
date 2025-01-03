86b010cf015882f310f5382ef87f947b8f598595
s0urce-c0de <email.kanavg@gmail.com>
2025-01-02 18:51:03 -0800

    I forgot -r

--- xargs_processor.sh
+++ xargs_processor.sh
@@ -2,5 +2,5 @@
 b64jq() { base64 -d | jq "$@"; }
 
 gh release list --json tagName | \
-jq -e "map(select(.tagName==\"$(printf "$1" | b64jq .file | awk -F '_' '{print $3}')/$(printf $1 | b64jq -r .channel)/v$(printf $1 | b64jq -r .version)\"))==[]" >/dev/null && \
+jq -e "map(select(.tagName==\"$(printf "$1" | b64jq -r .file | awk -F '_' '{print $3}')/$(printf $1 | b64jq -r .channel)/v$(printf $1 | b64jq -r .version)\"))==[]" >/dev/null && \
 env BOARD="$(printf "$1" | b64jq ".file" | awk -F _ '{print $3}')" $(printf $1 | b64jq -rcM '[. | to_entries | .[] | (.key | ascii_upcase) + "=" + .value] | join(" ")') ./process_info.sh
